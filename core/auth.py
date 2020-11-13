a1805a78ada8f8535f7ad98fe14c3cdece9a2a24
Julio <jn5.guape@gmail.com>
2020-11-12 21:26:39 -0300

    sistema de login

---
+++ core/auth.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import requests
+
+Api_dados = 'http://192.168.0.107:8000/api/'
+credenciais_api_dados = ("julio","15052005")
+
+class User():
+    id = None
+    nome = ''
+    toke = ''
+    cargo = ''
+    verificado = False
+
+    def check_acess(self, cargo_min = 'C'):
+        return self.cargo >= cargo_min
+
+    def login(self, email='',senha=''):
+        if self.id != None:
+            return {
+                "sucess":False,
+                "auth": False,
+                "conecao":True,
+                "error":"Usuario já autenticado.",
+            }
+        else:
+            r = 0
+            try:
+                r = requests.post(Api_dados+"login/",data={"email": email, "password": senha}, auth=credenciais_api_dados)
+            except:
+                return {
+                    "sucess":False,
+                    "auth": False,
+                    "conecao":False,
+                    "error":"Não foi possivel realizar conexão com o servidor",
+                }
+
+            if r.status_code != 200 and r.status_code != "200":
+                return {
+                    "sucess":False,
+                    "auth": False,
+                    "conecao":True,
+                    "error":f"Erro na requisição, erro: {str(r.status_code)}",
+                }
+            else:
+                data = r.json()
+                if data['sucess'] == True and data['auth'] == True:
+                    self.id = data['dados']['id']
+                    self.nome = data['dados']['nome']
+                    self.token = data['dados']['user_token']
+                    self.cargo = data['dados']['cargo']
+                    self.verificado = data['dados']['is_trusty']
+
+                    return {
+                        "sucess":True,
+                        "auth": True,
+                    }
+                else:
+                    return {
+                        "sucess":False,
+                        "auth": False,
+                        "conecao":True,
+                        "error":f"Não foi possivel realizar a autenticação, erro: {data['error']}",
+                    }
--- main.py
+++ main.py
@@ -1,12 +1,77 @@
+# -*- coding: utf-8 -*-
 import os
 import sys
 import requests
 import serial
+import json
 import platform
+from tqdm import tqdm
+from time import sleep
+from core.auth import User
 
+#for i in tqdm(range(1000)):
+#     sleep(0.01)
+
+user = User()
+so = platform.system()
+config = open('config.json','r')
+config = json.load(config)
+
+version = config['version']
+ano = config['ano']
+autor = config['autor']
+
+def login():
+    email = input('\nEmail: ')
+    senha = input('\nSenha: ')
+
+    r = user.login(email=email, senha=senha)
+    return r
+
+def start():
+    print(f'------- Bem vindo ao Serial-Config v{version}-------')
+    sleep(0.5)
+    print(f'-- Copyright (C) {ano} by {autor} --')
+    sleep(0.5)
+    print('----- Faça Login para utilizar o sistema -----')
+    sleep(0.5)
+    print('\n----------------------------------------------\n')
+    sleep(0.5)
+
+def log(menssagem):
+    print(f"[ INFO ] {menssagem}")
 
 def main():
-    pass
+    start()
+    r = input('Continuar (y/n): ')
+    if r != "y" and r != "Y":
+        return
+    while True:
+        r = login()
+        if r['sucess'] == False:
+            if r['conecao'] == True:
+                log(r['error'])
+                t = input('Tentar novamente ??? (y/n): ')
+                if t != "y" and t != "Y":
+                    log('Processo terminado pelo usuario')
+                    return
+                else:
+                    continue
+            else:
+                log(r['error'])
+                return
+        elif r['sucess'] == True:
+            log('Logado com sucesso!')
+            break
+        else:
+            log('Erro no sistema!')
+            return
+
+    if user.check_acess():
+        log('Acesso permitido')
+    else:
+        log('Usuario sem acesso ao sistema')
+        return
 
 if __name__ == '__main__':
     main()
